0836156ee7a3be1d62332b73e21551392de827c8
Bruno Vilardi Bueno <brunovilardibueno@gmail.com>
2023-11-29 23:20:12 -0300

    first lambda and lambda layer

--- iac/iac/iac_stack.py
+++ iac/iac/iac_stack.py
@@ -1,7 +1,10 @@
 from aws_cdk import (
     # Duration,
+    Duration,
     Stack,
     # aws_sqs as sqs,
+    aws_lambda as lambda_
+
 )
 from constructs import Construct
 
@@ -10,10 +13,16 @@ class IacStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "IacQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        self.lambda_layer = lambda_.LayerVersion(self, "LambdaLayer",
+                                                 code=lambda_.Code.from_asset("../lambda_layer"),
+                                                 compatible_runtimes=[lambda_.Runtime.PYTHON_3_9]
+                                                 )
+        
+        self.enviar_feedback_lambda = lambda_.Function(self, "EnviarFeedbackLambda",
+                                                         code=lambda_.Code.from_asset("../funcoes/enviar_feedback"),
+                                                         handler="enviar_feedback_lambda.lambda_handler",
+                                                         runtime=lambda_.Runtime.PYTHON_3_9,
+                                                         timeout=Duration.seconds(30),
+                                                         memory_size=128,
+                                                         layers=[self.lambda_layer],
+                                                         )
